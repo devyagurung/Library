4329405060e705c302831b0b5d50a04e1cda9cae
Devya <Devya@Devya-HP>
2015-05-19 20:48:07 -0400

    Library Books

--- Library/src/lab1/Book.java
+++ Library/src/lab1/Book.java
@@ -2,15 +2,16 @@ package lab1;
 
 /**
  * A representation of a book.
- * @author <a href="mailto:rkhatchadourian@citytech.cuny.edu">Raffi Khatchadourian</a>.
+ *
+ * @author Devya Gurung & Niaz Morshed
  */
 public class Book {
 
     /**
-     * This book's title.
+     * This book title.
      */
     private String title;
-    
+
     /**
      * True if this book is borrowed and false otherwise.
      */
@@ -18,40 +19,43 @@ public class Book {
 
     /**
      * Creates a new Book.
+     *
      * @param bookTitle The title of the book that will be created.
      */
     public Book(String bookTitle) {
-        // TODO: Implement this method
+        title = bookTitle;
     }
-   
-    /** 
+
+    /**
      * Marks this book as rented.
      */
     public void rented() {
-        // TODO: Implement this method
+        borrowed = true;
     }
-   
+
     /**
      * Marks this book as not rented.
      */
     public void returned() {
-        // TODO: Implement this method
+        borrowed = false;
     }
-   
+
     /**
      * Returns true if this book is rented, false otherwise.
+     *
      * @return true if this book is rented and false otherwise.
      */
     public boolean isBorrowed() {
-        // TODO: Implement this method
+        return borrowed;
     }
-   
-    /** 
-     * Returns this book's title as a String.
-     * @return This book's title as a String.
+
+    /**
+     * Returns this book title as a String.
+     *
+     * @return This book title as a String.
      */
     public String getTitle() {
-        // TODO: Implement this method
+        return title;
     }
 
     public static void main(String[] arguments) {
@@ -64,4 +68,4 @@ public class Book {
         example.returned();
         System.out.println("Borrowed? (should be false): " + example.isBorrowed());
     }
-} 
+}
